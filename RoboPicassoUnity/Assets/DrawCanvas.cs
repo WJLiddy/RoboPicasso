bcd33e619d6a4909288d3ca6a7ae91e7a15f11cd
WJLIDDY <wjl36@case.edu>
2017-10-07 16:36:20 -0400

    add painting

--- RoboPicassoUnity/Assets/DrawCanvas.cs
+++ RoboPicassoUnity/Assets/DrawCanvas.cs
@@ -1,15 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
 public class DrawCanvas : MonoBehaviour
 {
 
     SpriteRenderer canvas;
+    Texture2D canvas_texture;
     // Use this for initialization
     void Awake()
     {
@@ -24,14 +21,109 @@ public class DrawCanvas : MonoBehaviour
         // Resize
         canvas.transform.localScale = new Vector2(deviceWidth / 4, deviceWidth / 4);
 
-        // Move up. Center at .25 of height.
-        canvas.transform.localPosition = new Vector2(0, topRightCorner.y - (deviceWidth / 4));
+        // Center 
+        canvas.transform.localPosition = new Vector2(0, (deviceHeight / 2) - (deviceWidth / 2));
 
+        canvas_texture = GetComponent<SpriteRenderer>().sprite.texture;
+        clearCanvas();
     }
 
+    void clearCanvas()
+    {
+        for(int x = 0; x != 400; x++)
+        {
+            for(int y = 0; y != 400; y++)
+            {
+                canvas_texture.SetPixel(x, y, Color.white);
+            }
+        }
+    }
+    void ColorOnDrawCanvas(int paint_index, int x, int y)
+    {
+        
+        Color c = new Color(0,0,0);
+        switch(paint_index)
+        {
+            case -1: c = new Color(0, 0, 0); break;
+            case 0: c = new Color(1, 0, 0); break;
+            case 1: c = new Color(1, 0.5f, 0); break;
+            case 2: c = new Color(1, 1, 0); break;
+            case 3: c = new Color(0, 1, 0); break;
+            case 4: c = new Color(0, 0, 1); break;
+            case 5: c = new Color(0.5f, 0.5f, 0.5f); break;
+            case 6: c = new Color(0f, 0f, 0f); break;
+        }
+        Debug.Log(c);
+        canvas_texture.SetPixel(x, y, c);
+    }
+
+    void processSingleTouchEvent(Vector2 touch_vec)
+    {
+        //Add half of device width to offset from 0 and get ratio.
+        float x_fraction = touch_vec.x / (float)Screen.width;
+        // bias y with part of screen on bottom
+        float y_bias = -((Screen.height - Screen.width));
+        float y_fraction = (touch_vec.y + y_bias) / (float)Screen.width;
+
+        ToolsPanel t = GameObject.FindGameObjectWithTag("ToolsPanel").GetComponent<ToolsPanel>();
+
+        int center_x = (int)(x_fraction * 400);
+        int center_y = (int)(y_fraction * 400);
+        Debug.Log(center_y);
+
+
+        Debug.Log(center_x);
+        Debug.Log(center_y);
+
+        int BRUSH_SIZE = 2;
+        if (t.erase_mode)
+        {
+            //draw... 5 by 5?
+            for (int x = 0; x != BRUSH_SIZE; x++)
+            {
+                for (int y = 0; y != BRUSH_SIZE; y++)
+                {
+                    ColorOnDrawCanvas(-1, x, y);
+                }
+            }
+        }
+        else if (t.floodfill)
+        {
+
+        }
+        else
+        {
+            //draw... 5 by 5?
+            for (int x = -BRUSH_SIZE; x != BRUSH_SIZE; x++)
+            {
+                for (int y = -BRUSH_SIZE; y != BRUSH_SIZE; y++)
+                {
+                    ColorOnDrawCanvas(t.brush_selection, center_x + x, center_y + y);
+                }
+            }
+        }
+    }
+    void processTouchInputs()
+    {
+        if (!Application.isMobilePlatform)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                processSingleTouchEvent(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            }
+        }
+        else
+        {
+            for (var i = 0; i < Input.touchCount; ++i)
+            {
+                processSingleTouchEvent(Input.GetTouch(i).position);
+            }
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-
+        processTouchInputs();
+        canvas_texture.Apply();
     }
 }
--- RoboPicassoUnity/Assets/ToolsPanel.cs
+++ RoboPicassoUnity/Assets/ToolsPanel.cs
@@ -1,8 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ToolsPanel : MonoBehaviour {
+
+    public bool erase_mode = false;
+    public bool floodfill = false;
+
+    public int brush_selection = 6;
+    List<GameObject> paint_buttons = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +20,38 @@ public class ToolsPanel : MonoBehaviour {
         var deviceW = topRightCorner.x - origin.x;
         Debug.Log(deviceW);
         Debug.Log(deviceH);
+        var pct = (deviceH - deviceW) / deviceH;
+        Debug.Log(pct);
+        //Resize this based on space.
+        GetComponent<RectTransform>().anchorMax = new Vector2(1, pct);
 
-	}
+        //Get ROYGBgb btns.
+        for (int i = 0; i != 7; i++)
+        {
+            paint_buttons.Add(transform.GetChild(i).GetChild(0).gameObject);
+        }
+        brushSelection(6);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void brushSelection(int i)
+    {
+        brush_selection = i;
+        for(int j = 0; j != 7; j++)
+        {
+            if(j != brush_selection)
+            {
+                //disable paint icon
+                paint_buttons[j].GetComponentInChildren<Image>().color = new Color(0, 0, 0, 0);
+            } else
+            {
+                //enable paint icon
+                paint_buttons[j].GetComponentInChildren<Image>().color = new Color(255, 255, 255, 255);
+            }
+        }
+    }
 }
