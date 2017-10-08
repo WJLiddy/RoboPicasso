9b5e79bc7dd25800c4f74859dd6177189b28460e
CorruptData <mccargishm@gmail.com>
2017-10-07 23:51:11 -0400

    Jack REALLY LIKES Tabs

--- RoboPicassoServer/rating.py
+++ RoboPicassoServer/rating.py
@@ -15,31 +15,31 @@ from google.cloud.vision import types
 # Outputs -> Score (0 ~ 1)
 def rate(google_image_annotator_client, content, category):
 
-	# Performace hit? Maybe.
-	image = types.Image(content=content)
+  # Performace hit? Maybe.
+  image = types.Image(content=content)
 
-	# TODO Give the user a lot of leeway - try to detect up to 50 things. Right now gets 10.
-	response = google_image_annotator_client.label_detection(image=image)
-	labels = response.label_annotations
+  # TODO Give the user a lot of leeway - try to detect up to 50 things. Right now gets 10.
+  response = google_image_annotator_client.label_detection(image=image)
+  labels = response.label_annotations
 
-	guessed_labels = []
+  guessed_labels = []
         print("LABEL LEN")
         print(len(labels))
-	for label in labels:
-		if(label.description == category):
-			return label.score
-	return 0
+  for label in labels:
+    if(label.description == category):
+      return label.score
+  return 0
 
 
 def test():
-	client = vision.ImageAnnotatorClient()
-	files = ["barlyke.png","shitty_car.png","good_car.png"]
-	imgs = []
-	for file in files:
-		with io.open("tests/" + file, 'rb') as image_file:
-			imgs.append(image_file.read())
+  client = vision.ImageAnnotatorClient()
+  files = ["barlyke.png","shitty_car.png","good_car.png"]
+  imgs = []
+  for file in files:
+    with io.open("tests/" + file, 'rb') as image_file:
+      imgs.append(image_file.read())
 
-	for image in imgs:
-		print(rate(client,image,self.prompt))
+  for image in imgs:
+    print(rate(client,image,self.prompt))
 
 test()
